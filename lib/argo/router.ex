e8935e313e983f37c1b84dcfca8bcb8cecd8ddf0
Jindrich K. Smitka <jsmitka@smita.info>
2019-07-10 13:23:01 +0200

    Refactored template loading

--- lib/argo/router.ex
+++ lib/argo/router.ex
@@ -3,13 +3,11 @@ defmodule Argo.Router do
 
   alias Plug.Conn
 
-  defmacro __using__(opts) do
+  defmacro __using__(_opts) do
     quote do
       use Plug.Router
 
       alias Argo.Router
-
-      @template_root Path.relative_to_cwd(unquote(opts)[:template_root])
 
       plug(:match)
       plug(:dispatch)
@@ -19,7 +17,7 @@ defmodule Argo.Router do
   defmacro get(path, module, function) do
     quote do
       get unquote(path) do
-        Argo.Router.process(var!(conn), @template_root, unquote(module), unquote(function))
+        Argo.Router.process(var!(conn), unquote(module), unquote(function))
       end
     end
   end
@@ -27,7 +25,7 @@ defmodule Argo.Router do
   defmacro post(path, module, function) do
     quote do
       post unquote(path) do
-        Argo.Router.process(var!(conn), @template_root, unquote(module), unquote(function))
+        Argo.Router.process(var!(conn), unquote(module), unquote(function))
       end
     end
   end
@@ -35,7 +33,7 @@ defmodule Argo.Router do
   defmacro put(path, module, function) do
     quote do
       put unquote(path) do
-        Argo.Router.process(var!(conn), @template_root, unquote(module), unquote(function))
+        Argo.Router.process(var!(conn), unquote(module), unquote(function))
       end
     end
   end
@@ -43,7 +41,7 @@ defmodule Argo.Router do
   defmacro patch(path, module, function) do
     quote do
       patch unquote(path) do
-        Argo.Router.process(var!(conn), @template_root, unquote(module), unquote(function))
+        Argo.Router.process(var!(conn), unquote(module), unquote(function))
       end
     end
   end
@@ -51,7 +49,7 @@ defmodule Argo.Router do
   defmacro delete(path, module, function) do
     quote do
       delete unquote(path) do
-        Argo.Router.process(var!(conn), @template_root, unquote(module), unquote(function))
+        Argo.Router.process(var!(conn), unquote(module), unquote(function))
       end
     end
   end
@@ -59,15 +57,33 @@ defmodule Argo.Router do
   defmacro options(path, module, function) do
     quote do
       options unquote(path) do
-        Argo.Router.process(var!(conn), @template_root, unquote(module), unquote(function))
+        Argo.Router.process(var!(conn), unquote(module), unquote(function))
       end
     end
   end
 
-  def process(conn, template_dir, module, function) do
+  def process(conn, module, function) do
     conn
     |> Controller.call(module, function)
-    |> View.render(template_dir)
+    |> View.render(view_name_from_controller(module))
     |> Conn.send_resp()
+  end
+
+  defp view_name_from_controller(controller_name) when is_atom(controller_name) do
+    controller_name
+    |> unsuffix("Controller")
+    |> Kernel.<>("View")
+    |> String.to_existing_atom()
+  end
+
+  defp unsuffix(value, suffix) do
+    string = to_string(value)
+    suffix_size = byte_size(suffix)
+    prefix_size = byte_size(string) - suffix_size
+
+    case string do
+      <<prefix::binary-size(prefix_size), ^suffix::binary>> -> prefix
+      _ -> string
+    end
   end
 end
--- lib/argo/view.ex
+++ lib/argo/view.ex
@@ -5,12 +5,35 @@ defmodule Argo.View do
 
   alias Plug.Conn
 
-  def render(%Conn{private: %{pages_render: {template, assigns}}} = conn, templates_path) do
-    content =
-      templates_path
-      |> Path.join("#{template}.eex")
-      |> EEx.eval_file(Keyword.put(assigns, :conn, conn))
+  defmacro __using__(opts) do
+    templates = load_templates(Path.relative_to_cwd(opts[:template_root]))
 
-    Conn.resp(conn, 200, content)
+    quote do
+      @templates unquote(Macro.escape(templates))
+
+      def render_template(template, assigns) do
+        @templates
+        |> Map.get(template, nil)
+        |> EEx.eval_string(assigns)
+      end
+    end
+  end
+
+  def render(%Conn{private: %{pages_render: {template, assigns}}} = conn, view_module) do
+    content = apply(view_module, :render_template, [template, Keyword.put(assigns, :conn, conn)])
+
+    conn
+    |> Conn.put_resp_header("content-type", "text/html")
+    |> Conn.resp(200, content)
+  end
+
+  defp load_templates(path) do
+    path
+    |> File.ls!()
+    |> Enum.map(fn file -> {Path.rootname(file), Path.extname(file)} end)
+    |> Enum.filter(fn {_, ext} -> ext == ".eex" end)
+    |> Enum.reduce(%{}, fn {k, ext}, acc ->
+      Map.put(acc, k, path |> Path.join(k <> ext) |> File.read!())
+    end)
   end
 end
