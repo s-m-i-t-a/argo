afb629c3cd91353ffe29cc3494fecad6e41cbab4
Jindrich K. Smitka <jsmitka@smita.info>
2019-10-04 20:53:25 +0200

    Bump version: 0.2.1 → 0.2.2

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 ```elixir
 def deps do
   [
-    {:argo, "~> 0.2.1"}
+    {:argo, "~> 0.2.2"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Argo.MixProject do
   def project do
     [
       app: :argo,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
