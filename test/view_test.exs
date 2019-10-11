0a53453de8f31480892eb8df17b8f93b511e36a4
Jindrich K. Smitka <jsmitka@smita.info>
2019-10-11 11:35:22 +0200

    Added loading templates from subdirs

---
+++ test/view_test.exs
@@ -0,0 +1,17 @@
+defmodule ViewTest do
+  @moduledoc false
+  use ExUnit.Case
+  doctest Argo.View
+
+  alias Argo.View
+
+  defmodule TestView do
+    use Argo.View, template_root: "test/templates"
+  end
+
+  test "should load templates from subdirectories" do
+    rv = TestView.render_template("one/test.html", [])
+
+    assert rv =~ ~r/<h1>One<\/h1>/
+  end
+end
